bbeb67c5b304f6fdae8377c5281b3f669c38658a
CodeBlack0 <tim.fischer98@hotmail.com>
2017-07-16 22:56:55 +0200

    changed plotting

--- stock_anal.py
+++ stock_anal.py
@@ -2,12 +2,15 @@ import csv
 import os
 import inspect
 import time
-import pandas as pd
-from math import pi
-from bokeh.plotting import figure, show, output_file
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 from collections import namedtuple
 from datetime import date
 from contextlib import contextmanager
+
+years = mdates.YearLocator()   # every year
+months = mdates.MonthLocator()  # every month
+yearsFmt = mdates.DateFormatter('%Y')
 
 
 def import_data(path):
@@ -77,43 +80,48 @@ def load_data(path, converter=convert_price_data, target='prices'):
 def timethis(label):
     '''Simple context manager to time things'''
     start = time.time()
+    print(f'starting: {label}')
     try:
         yield
     finally:
         end = time.time()
-        print(f'{label}: {end - start}')
+        print(f'end: {label} with {end - start}')
 
 
-def plot(symbol='AAPL'):
-    with timethis(f'loadind data for {symbol}: '):
-        spec_data = [val for val in chain_data(generateDataDict('./data'))
-                     if val.symbol == symbol]
-    df = pd.DataFrame(columns=['date', 'open', 'close', 'high', 'low'])
-    for item in spec_data:
-            df.add([item.date, item.open, item.close,
-                    item.high, item.low])
-    df['date'] = pd.to_datetime(df['date'])
-
-    inc = df.close > df.open
-    dec = df.open > df.close
-    w = 43200000  # half day in ms
-
-    TOOLS = "pan,wheel_zoom,box_zoom,reset,save"
-
-    p = figure(x_axis_type="datetime", tools=TOOLS,
-               plot_width=1000, title=f'{symbol} Candlestick')
-    p.xaxis.major_label_orientation = pi / 4
-    p.grid.grid_line_alpha = 0.3
-
-    p.segment(df.date, df.high, df.date, df.low, color="black")
-    p.vbar(df.date[inc], w, df.open[inc], df.close[inc],
-           fill_color="#D5E1DD", line_color="black")
-    p.vbar(df.date[dec], w, df.open[dec], df.close[dec],
-           fill_color="#F2583E", line_color="black")
-
-    output_file("candlestick.html", title="candlestick.py example")
-
-    show(p)
+def write_all(path='all.csv'):
+    with timethis('consildating data'):
+        with load_data('./data') as data:
+            with open(path, 'w') as f:
+                row = next(data)
+                writer = csv.writer(f, quoting=csv.QUOTE_NONE)
+                writer.writerow([x for x in row._fields])
+                writer.writerow([getattr(row, x) for x in row._fields])
+                for row in data:
+                    writer.writerow([getattr(row, x) for x in row._fields])
 
 
-plot()
+def plot(symbol='AAPL', path='all.csv', ax=None, toplot='adj_close'):
+    if not ax:
+        fig, ax = plt.subplots()
+    ax.set_title(symbol + " Stocks")
+
+    def price(x):
+        return '$%1.2f' % x
+    ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
+    ax.format_ydata = price
+    ax.grid(True)
+    with timethis('loading data'):
+        plotting_data_X = list()
+        plotting_data_Y = list()
+        for item in convert_price_data(import_data(path)):
+            if item.symbol == symbol:
+                plotting_data_Y.append(getattr(item, toplot))
+                plotting_data_X.append(item.date)
+    try:
+        ax.plot(plotting_data_X, plotting_data_Y, 'o')
+        plt.show()
+    except Exception as e:
+        print(e)
+
+
+plot(symbol='INTL')
